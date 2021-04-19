39e32a8ae8270af14046c9ffcda42466813816d3
Lizz Ultee [air] <ehultee@umich.edu>
2021-04-19 17:09:39 -0400

    Figure 5 refinements
    Adjust labelling, tick positioning and scaling.  Add mean ice flow speed for context along the flowline.

--- Manuscript-figures/2d-xcorr_along_flow.py
+++ Manuscript-figures/2d-xcorr_along_flow.py
@@ -10,6 +10,20 @@ import nifl_helper
 from matplotlib import cm
 from matplotlib.ticker import FixedLocator
 
+## set matplotlib font size defaults
+SMALL_SIZE = 10
+MEDIUM_SIZE = 11
+BIGGER_SIZE = 14
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
 
 ## Extract bed topo along flowline
 xyvals = np.array([(xh[i], yh[i]) for i in range(len(xh))])
@@ -17,14 +31,21 @@ bed_vals = [float(B_helheim(xh[i], yh[i])) for i in range(len(xh))]
 surface_vals = [float(S_helheim(xh[i], yh[i])) for i in range(len(xh))]
 xvals = (0.001*np.array(nifl_helper.ArcArray(xyvals)))
 
+
+## Compute mean ice speed for each point
+mean_speed = [np.mean(pred['full']) for pred in preds]
+scaled_speed = [m/np.mean(mean_speed) for m in mean_speed]
+scaled_ticks = (np.array([4.5, 6, 7.5])/np.mean(mean_speed)) - np.mean(scaled_speed) # for display
+
+
 ## Create arclength array for selected points
 s = 0.001*np.array(nifl_helper.ArcArray(np.array(xys)))+2 # trim the 2km that was removed
 
-
 ### Plot all on one set of axes
-smb_offset=7000 # how many 'meters' above topo to plot this line
-runoff_offset=5000 
-terminus_offset=3000
+smb_offset=9000 # how many 'meters' above topo to plot this line
+runoff_offset=7000 
+terminus_offset=5000
+velocity_offset=2500 
 scaling=1000  # vertical multiplicative factor to display xcorr on same axes as topo
 qual_colors = cm.get_cmap('tab20b')
 
@@ -48,38 +69,37 @@ ax.plot(s, smb_offset+ scaling*np.array(smb_lt_corr_amax), color=qual_colors(1),
 ax.plot(s, runoff_offset+ scaling*np.array(rf_lt_corr_amax), color=qual_colors(3), ls=':', lw=2.5, label='Runoff long-term')
 ax.plot(s, terminus_offset+ scaling*np.array(term_lt_corr_amax), color=qual_colors(5), ls=':', lw=2.5, label='Terminus long-term')
 
-# ## fill to show sign
-# plt.fill_between(s, smb_offset+ scaling*np.array(smb_corr_amax), y2=smb_offset, 
-#                  where=smb_significance, color=qual_colors(0), alpha=0.4)
-# plt.fill_between(s, runoff_offset+ scaling*np.array(runoff_corr_amax), y2=runoff_offset, 
-#                  where=runoff_significance, color=qual_colors(2), alpha=0.4)
-# plt.fill_between(s, terminus_offset+ scaling*np.array(terminus_corr_amax), y2=terminus_offset, 
-#                  where=terminus_significance, color=qual_colors(4), alpha=0.4)
+## add velocity
+ax.plot(s, velocity_offset+scaling*np.array(scaled_speed - np.mean(scaled_speed)), color=qual_colors(17), label='Mean speed', lw=2.5)
 
 
 ax.plot(xvals, bed_vals, color='saddlebrown', lw=2.0)
 ax.plot(xvals, surface_vals, color='darkgrey', lw=2.0)
 plt.fill_between(xvals, surface_vals, bed_vals, color='darkgrey', alpha=0.5)
 plt.fill_between(xvals, bed_vals, y2=-1300, color='saddlebrown', alpha=0.5, hatch='/')
-ax.set(xlim=(25, 0), ylim=(-1300, 8000), aspect=0.002, 
+ax.set(xlim=(25, 0), ylim=(-1300, 10000), aspect=0.002, 
         xlabel='Upstream distance [km]',
         yticks=(-1000, 0, 1000, 
+                velocity_offset+scaling*scaled_ticks[1],
                 terminus_offset,  
                 runoff_offset, 
                 smb_offset),
         yticklabels=('-1000', '0', '1000 m a.s.l.', 
+                     'Surface speed',
                      'Terminus',
                      'Runoff',
                      'SMB'))
-minor_locator = FixedLocator([terminus_offset-0.5*scaling, terminus_offset+0.5*scaling, 
+minor_locator = FixedLocator([velocity_offset+scaling*scaled_ticks[0], velocity_offset+scaling*scaled_ticks[2],
+                              terminus_offset-0.5*scaling, terminus_offset+0.5*scaling, 
                               runoff_offset-0.5*scaling, runoff_offset+0.5*scaling,
                               smb_offset-0.5*scaling, smb_offset+0.5*scaling])
 ax.yaxis.set_minor_locator(minor_locator)
 ax.tick_params(axis='both', which='major', length=4)
 ax.tick_params(axis='y', which='minor', length=2)
-ax.get_yticklabels()[3].set_color(qual_colors(4)) # match the ticks to the plotted color
-ax.get_yticklabels()[4].set_color(qual_colors(2))
-ax.get_yticklabels()[5].set_color(qual_colors(0))
+ax.get_yticklabels()[3].set_color(qual_colors(17)) # match the ticks to the plotted color
+ax.get_yticklabels()[4].set_color(qual_colors(4)) 
+ax.get_yticklabels()[5].set_color(qual_colors(2))
+ax.get_yticklabels()[6].set_color(qual_colors(0))
 
 
 ax.spines['top'].set_visible(False)
